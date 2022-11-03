8b3283d23625b0f8ae10d9c0727a64c652b467ac
LuisSoqui <luisangelsoqui@outlook.com>
2022-11-03 10:54:30 -0700

    project server gateway

---
+++ springboot-servicio-gateway-server/src/test/java/com/mx/lsp/app/gateway/SpringServicioGatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mx.lsp.app.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringServicioGatewayServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
